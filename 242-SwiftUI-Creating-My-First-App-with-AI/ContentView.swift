ab1d715787d0e9680c3002878701b9a29fac21d6
Peter Pan <apppeterpan@gmail.com>
2025-09-08 15:57:41 +0800

    Initial Commit

---
+++ 242-SwiftUI-Creating-My-First-App-with-AI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  242-SwiftUI-Creating-My-First-App-with-AI
+//
+//  Created by SHIH-YING PAN on 2025/9/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 242-SwiftUI-Creating-My-First-App-with-AI/_42_SwiftUI_Creating_My_First_App_with_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _42_SwiftUI_Creating_My_First_App_with_AIApp.swift
+//  242-SwiftUI-Creating-My-First-App-with-AI
+//
+//  Created by SHIH-YING PAN on 2025/9/8.
+//
+
+import SwiftUI
+
+@main
+struct _42_SwiftUI_Creating_My_First_App_with_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
